bf2d9f6c03a05cef6d59e196920844bc1f9dca43
Nepetalactone <tobias.meusburger@gmx.net>
2015-02-12 19:23:27 +0100

    include probability of letters

---
+++ 00NameGenerator/00NameGenerator/ProbabilityList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _00NameGenerator
+{
+    class ProbabilityList<T>
+    {
+        private readonly List<T> _list;
+
+        public ProbabilityList()
+        {
+            _list = new List<T>();
+        }
+
+        public void Add(T item, Double probability)
+        {
+            for (int i = 0; i < probability * 10; i++)
+            {
+                _list.Add(item);
+            }
+        }
+
+        public void Remove(T item)
+        {
+            _list.RemoveAll(obj => obj.Equals(item));
+        }
+
+        public T GetRandomItem()
+        {
+            Random rand = new Random();
+            return _list[rand.Next(_list.Count)];
+        }
+    }
+}
--- 00NameGenerator/00NameGenerator/Program.cs
+++ 00NameGenerator/00NameGenerator/Program.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _00NameGenerator
 {
     class Program
     {
-        private static readonly char[] Vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
-        private static readonly string[] Diphtongs = new string[5] { "ai", "au", "eu", "ei", "ui" };
-        private static readonly char[] Consonants = new char[21] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
-        private static readonly Random Rng = new Random();
+
+        private static ProbabilityList<char> _vowels;
+        private static ProbabilityList<string> _diphthongs;
+        private static ProbabilityList<char> _consonants;
+        private static Random _rng;
 
         private static void Main(string[] args)
         {
+            InitCharLists();
+            _rng = new Random();
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine(GenerateName() + Environment.NewLine);
@@ -25,25 +29,24 @@ namespace _00NameGenerator
 
         private static String GenerateName()
         {
-            int numOfParts = Rng.Next(3, 10);
-            int poolChoice = Rng.Next(0, 6);
+            int numOfParts = _rng.Next(3, 6);
+            int poolChoice = _rng.Next(0, 6);
             StringBuilder generatedName = new StringBuilder();
 
             //Appends an entry from the appropriate letter pool and afterwards changes the pool to choose from 
             for (int i = 0; i < numOfParts; i++)
             {
-                int letterChoice;
                 switch (poolChoice)
                 {
                     case 0:
-                        letterChoice = Rng.Next(0, 5);
-                        generatedName.Append(Vowels[letterChoice]);
+                        Thread.Sleep(20);
+                        generatedName.Append(_vowels.GetRandomItem());
                         poolChoice = 2;
                         break;
 
                     case 1:
-                        letterChoice = Rng.Next(0, 5);
-                        generatedName.Append(Diphtongs[letterChoice]);
+                        Thread.Sleep(20);
+                        generatedName.Append(_diphthongs.GetRandomItem());
                         poolChoice = 2;
                         break;
                     //Give consonants a higher chance to appear
@@ -51,9 +54,9 @@ namespace _00NameGenerator
                     case 3:
                     case 4:
                     case 5:
-                        letterChoice = Rng.Next(0, 21);
-                        generatedName.Append(Consonants[letterChoice]);
-                        poolChoice = Rng.Next(0, 2);
+                        Thread.Sleep(20);
+                        generatedName.Append(_consonants.GetRandomItem());
+                        poolChoice = _rng.Next(0, 2);
                         break;
                 }
             }
@@ -61,6 +64,46 @@ namespace _00NameGenerator
             generatedName[0] = Char.ToUpper(generatedName[0]);
 
             return generatedName.ToString();
+        }
+
+        private static void InitCharLists()
+        {
+            _vowels = new ProbabilityList<char>();
+            _vowels.Add('a', 8.1);
+            _vowels.Add('e', 12.7);
+            _vowels.Add('i', 6.9);
+            _vowels.Add('o', 7.5);
+            _vowels.Add('u', 2.7);
+
+            _diphthongs = new ProbabilityList<string>();
+            _diphthongs.Add("ai", 20);
+            _diphthongs.Add("au", 20);
+            _diphthongs.Add("eu", 20);
+            _diphthongs.Add("ei", 20);
+            _diphthongs.Add("ui", 20);
+
+            _consonants = new ProbabilityList<char>();
+            _consonants.Add('b', 1.4);
+            _consonants.Add('c', 2.7);
+            _consonants.Add('d', 4.2);
+            _consonants.Add('f', 2.2);
+            _consonants.Add('g', 2.0);
+            _consonants.Add('h', 6.0);
+            _consonants.Add('j', 0.1);
+            _consonants.Add('k', 0.7);
+            _consonants.Add('l', 4.0);
+            _consonants.Add('m', 2.4);
+            _consonants.Add('n', 6.7);
+            _consonants.Add('p', 1.9);
+            _consonants.Add('q', 0.1);
+            _consonants.Add('r', 5.9);
+            _consonants.Add('s', 6.3);
+            _consonants.Add('t', 9.0);
+            _consonants.Add('v', 0.9);
+            _consonants.Add('w', 2.3);
+            _consonants.Add('x', 0.1);
+            _consonants.Add('y', 1.9);
+            _consonants.Add('z', 0.1);
         }
     }
 }
